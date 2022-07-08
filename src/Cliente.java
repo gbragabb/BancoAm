624ba5d97d7ac63be1c50abd3c7cfec0f89ec27d
F1705096 Caio Clementino Lamarao <caio.lamarao@bb.com.br>
2022-07-08 20:49:37 -0300

    Alteração inicial Aplicação Alteração clientes, pf e pj, tratamento inclusão cliente & conta

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,29 +1,16 @@
-import java.util.Scanner;
 
 public abstract class Cliente {
-    private String telefone;
 
-    protected void setTelefone(String telefone) {
+    private String telefone;
+    
+    public Cliente(String telefone){
+
         this.telefone = telefone;
     }
+    public String getTelefone() {
 
-    public abstract String getCadastro();
-
-    public abstract String getNome();
-
-    public Conta abrirConta(int escolha){
-        Conta c;
-        switch (escolha){
-            case 2:
-                c = new Poupanca(this);
-                break;
-            case 3:
-                c = new Investimento(this);
-                break;
-            default:
-                c = new Corrente(this);
-                break;
-        }
-        return c;
+        return this.telefone;
     }
+
+    public abstract Conta abrirConta();
 }
--- src/PF.java
+++ src/PF.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class PF extends Cliente{
     private String nome;
     private String cpf;
 
-    public PF(String nome, String cpf){
+    public PF(String nome, String cpf, String telefone){
+        super(telefone);
         this.cpf = cpf;
         this.nome = nome;
     }
@@ -13,5 +16,26 @@ public class PF extends Cliente{
 
     public String getCadastro() {
         return cpf;
+    }
+
+    @Override
+    public Conta abrirConta() {       
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual conta deseja criar?");
+		
+		System.out.println("<C>orrente - <P>oupanca - <I>nvetimento");
+		    char tipo = sc.nextLine().toLowerCase().charAt(0);
+		
+        sc.close();
+        
+        if (tipo == 'p')
+            return new Poupanca(this);
+
+        else if (tipo == 'i')
+            return new Investimento(this);
+            
+        else
+            return new Corrente(this);
     }
 }
